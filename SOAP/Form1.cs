2e20d623d33d261e2bf5641a324c12e68ad5b8d5
Tothadam80 <toth.adam80@gmail.com>
2021-10-27 22:01:30 +0200

    task 6 done

--- SOAP/Form1.cs
+++ SOAP/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace SOAP
@@ -25,6 +26,22 @@ namespace SOAP
             dataGridView1.DataSource = rates;
 
             CreateXML(result);
+
+            chartRateData.DataSource = rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
         private void CreateXML(string result)
